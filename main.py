46278e7010095efc07493291862cfbbebf9bb53b
majsburken <annabark1@gmail.com>
2025-11-26 09:30:33 +0100

    lemming

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pandas as pd
 from summa import keywords
 import nltk
 from nltk.stem import PorterStemmer
+import spacy
 
 ps = PorterStemmer()
 
+nlp = spacy.load("en_core_web_sm")
 
 pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_rows', None)
@@ -15,7 +17,7 @@ data = pd.read_csv('Social_Media_Engagement_Dataset.csv')
 
 #print(data[['text_content', 'timestamp']])
 
-data = data[['timestamp', 'day_of_week', 'location', 'text_content', 'engagement_rate']].head(4)
+data = data[['timestamp', 'day_of_week', 'location', 'text_content', 'engagement_rate']].head(20)
 
 data['text_content'] = data['text_content'].str.replace(r'[^A-Za-z0-9\s]', '', regex=True)
 data['text_content'] = data['text_content'].str.lower()
@@ -34,12 +36,23 @@ def stemming(keywords):
   #print(type(keyword))
   tokens = keywords.replace('\n',' ').split()
   #keywords.split(' ')
-  stems = [ps.stem(token)for token in tokens]
-  print(stems)
+  stems = []
+
+
+  for token in tokens:
+      word = nlp(token)
+      print(word)
+      for w in word:
+        stems.append(w.lemma_)
+    
+  #print(stems)
 
   return stems
 
-print(ps.stem('ran'))
+doc = nlp('ran')
+
+for d in doc:
+    print(d,d.lemma_)
 
 data['keywords'] = data['text_content'].apply(extract_keywords) #borde vara array av stringar
 
