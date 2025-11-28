4929765cc026aa5f64b7116d886f49e7e72b97c8
WilhelmSelestam <selestamw@gmail.com>
2025-11-28 17:03:42 +0100

    Embedding outputs

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ data = pd.read_csv('Social_Media_Engagement_Dataset.csv')
 
 #print(data[['text_content', 'timestamp']])
 
-data = data[['timestamp', 'day_of_week', 'location', 'text_content', 'engagement_rate']].head(2000)
+data = data[['timestamp', 'day_of_week', 'location', 'text_content', 'engagement_rate']]
 
 data['text_content'] = data['text_content'].str.replace(r'[^A-Za-z0-9\s]', '', regex=True)
 data['text_content'] = data['text_content'].str.lower()
@@ -30,8 +30,8 @@ data['text_content'] = data['text_content'].str.lower()
 #date format things
 date_format = '%Y-%m-%d %H:%M:%S'
 
-data = data.sort_values(by=['timestamp']).head(20)
-print(datetime.strptime(data['timestamp'].iloc[0],date_format))
+data = data.sort_values(by=['timestamp'])
+#print(datetime.strptime(data['timestamp'].iloc[0],date_format))
 
 
 def extract_keywords(text):
@@ -51,7 +51,7 @@ def lemming(keywords):
 
   for token in tokens:
       word = nlp(token)
-      print(word)
+      #print(word)
       for w in word:
         stems.append(w.lemma_)
     
@@ -61,17 +61,44 @@ def lemming(keywords):
 
 doc = nlp('ran')
 
-for d in doc:
-    print(d,d.lemma_)
+#for d in doc:
+    #print(d,d.lemma_)
 
 data['keywords'] = data['text_content'].apply(extract_keywords) #borde vara array av stringar
 
 data['keywords'] = data['keywords'].apply(lemming)
 
-print(data['keywords'])
+#print(data['keywords'])
 
 #embedding
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
+keywords = set()
+for row in data.itertuples(index=False):
+    for keyword in row.keywords:
+        #print(keyword)
+        keywords.add(keyword)
+
+keywords = list(keywords)
+print(keywords[95])
+
+# keywords = [
+#     "buy car", "purchase automobile", "buy vehicle", 
+#     "apple", "banana", "buy cars", "apple fruit"
+# ]
+# embeddings = model.encode(keywords)
+
+# clusters = util.community_detection(
+#     embeddings, 
+#     threshold=0.75, 
+#     min_community_size=1
+# )
+
+# print(clusters)
+
+# filtered_keywords = [keywords[cluster[0]] for cluster in clusters]
+
+# print(filtered_keywords)
 
 #week bins
 firsTimeInBin = datetime.strptime(data['timestamp'].iloc[0], date_format)
@@ -90,28 +117,28 @@ nr_of_weeks = math.ceil(time_span.days / 7)
 for week in range (0, nr_of_weeks - 1):
     topicSeries.append({})
 
-print(nr_of_weeks)
+#print(nr_of_weeks)
 
 current_week = 0
 week_length = timedelta(days=7)
 
-for row in data.itertuples(index=False):
+# for row in data.itertuples(index=False):
 
-    current_date = datetime.strptime(row.timestamp, date_format)
-    #print()  
+#     current_date = datetime.strptime(row.timestamp, date_format)
+#     #print()  
 
-    if current_date < start_time + week_length * (current_week + 1):
-        for keyword in row.keywords:
-            topicSeries[current_week][keyword] = topicSeries[current_week].get(keyword, 0) + 1
-        #topicSeries[current_week].append(row)
+#     if current_date < start_time + week_length * (current_week + 1):
+#         for keyword in row.keywords:
+#             topicSeries[current_week][keyword] = topicSeries[current_week].get(keyword, 0) + 1
+#         #topicSeries[current_week].append(row)
         
-    else:
-        current_week += 1
-        #topicSeries.append([row])
-        for keyword in row.keywords:
-            topicSeries[current_week][keyword] = topicSeries[current_week].get(keyword, 0) + 1
+#     else:
+#         current_week += 1
+#         #topicSeries.append([row])
+#         for keyword in row.keywords:
+#             topicSeries[current_week][keyword] = topicSeries[current_week].get(keyword, 0) + 1
 
 
 
-print(topicSeries)
+#print(topicSeries)
 
